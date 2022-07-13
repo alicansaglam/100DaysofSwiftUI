fcf19fb356e305ec1e103e1e6f09d382d215ed92
ALİ CAN SAGLAM <40038623+alicansaglam@users.noreply.github.com>
2022-07-13 17:57:20 +0300

    The first actual checkpoint is done

---
+++ Checkpoint-2/Checkpoint-2.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+//var greeting = "Hello, playground"
+
+
+let carBrands: [String] = ["Toyota", "Fiat", "Audi", "Ford", "Fiat", "Renault" , "Mercedes", "BMW", "Toyota"]
+let uniBrands = Set(carBrands)
+print("Total Brands Count: " + String(carBrands.count) + " and Unique Brands Count: " + String(uniBrands.count))
+
+
+
